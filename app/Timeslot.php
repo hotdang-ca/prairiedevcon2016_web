e58f1050aa97ead33f563a5a94b1a6a0b18e00a8
James Perih <james@hotdang.ca>
2016-03-31 09:24:50 -0500

    New Models

---
+++ app/Timeslot.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timeslot extends Model
+{
+    // has one speaker
+    public function speaker() {
+      return $this->belongsTo('App\Speaker');
+    }
+
+    // has one session
+    public function session() {
+      return $this->belongsTo('App\Sesssion');
+    }
+
+    // has one room
+    public function room() {
+      return $this->belongsTo('App\Room');
+    }
+}
